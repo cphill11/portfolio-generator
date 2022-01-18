b910d4a60ddad7f0a73b5fd8df69bb8192282f0c
Criste Phillips <criste.phillips@yahoo.com>
2022-01-18 17:42:18 -0500

    HTML generator code check

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// core code (const fs = require('fs);)
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 console.log(profileDataArgs);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
