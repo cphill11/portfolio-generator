5a72ef64e5966f2b9fdf50f6e17fa3255a3abbe4
Criste Phillips <criste.phillips@yahoo.com>
2022-01-20 11:11:44 -0500

    validate answers

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
-        // // validate: nameInput => {
-        //     if (nameInput) {
-        //         return true;
-        //     } else {
-        //         console.log('Please enter your name!');
-        //         return false;
-        //     }
-        //     }
-    //     }
+        message: 'What is your name?',
+        // if / else will require a name input and not allow a user to simply skip
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            
+            }
+        }
         },
       {
         type: 'input',
@@ -26,9 +27,22 @@ const promptUser = () => {
         message: 'Enter your GitHub Username'
       },
       {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout}) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
       }
     ]);
   };
